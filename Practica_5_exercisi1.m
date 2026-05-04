793926a19a808ab6c530d734cc73a9432d4db625
Anorlondor77 <anorlondorghotan@gmail.com>
2026-05-04 18:28:19 +0200

    Corrige Pràctica 5: MATLAB básico y múltiples iteraciones

--- Practica_5_exercisi1.m
+++ Practica_5_exercisi1.m
@@ -3,91 +3,44 @@ clear all
 close all
 clc
 
-%% PRACTICA 5 - PROBABILITAT
-% Aquest script resol els dos exercicis de simulacio de la practica.
+% EXERCICI 1
+% SIMULACIÓ DE PROBABILITATS DE PROCESSOS INDEPENDENTS
 
-%% EXERCICI 1
-% Simulacio de probabilitats de processos independents
-% M1 funciona amb probabilitat 0.6
-% M2 funciona amb probabilitat 0.7
-% Event A: les dues maquines funcionen
-% Event B: almenys una maquina funciona
+valors_numsims = [10 100 1000 10000];
 
-numsims = 10000;  % Podeu provar 10, 100, 1000, 10000
+for idx = 1:length(valors_numsims)
+    numsims = valors_numsims(idx);
 
-comptA = 0;
-comptB = 0;
+    % 1.1 Inicialització de comptadors
+    comptadorA = 0;
+    comptadorB = 0;
 
-for k = 1:numsims
-    M1 = rand;
-    M2 = rand;
+    % 1.3 Bucle principal
+    for i = 1:numsims
+        % 1.4 Generació de nombres aleatoris uniformes
+        M1 = rand();
+        M2 = rand();
 
-    funcionaM1 = (M1 < 0.6);
-    funcionaM2 = (M2 < 0.7);
+        % 1.5 Condicions de funcionament
+        funcionaM1 = (M1 < 0.6);
+        funcionaM2 = (M2 < 0.7);
 
-    if funcionaM1 && funcionaM2
-        comptA = comptA + 1;
-    end
-
-    if funcionaM1 || funcionaM2
-        comptB = comptB + 1;
-    end
-end
-
-probA = 100 * comptA / numsims;
-probB = 100 * comptB / numsims;
-
-fprintf('EXERCICI 1\n');
-fprintf('numsims = %d\n', numsims);
-fprintf('P(A: les dues maquines funcionen) = %.4f %%\n', probA);
-fprintf('P(B: almenys una maquina funciona) = %.4f %%\n\n', probB);
-
-%% EXERCICI 2
-% Simulacio de variables discretes i la seva probabilitat
-% Nombre de clients/hora ~ Poisson(5)
-% Cada client envia 1,2,3,4 paquets amb probabilitats 0.4,0.3,0.2,0.1
-% Event A: s'envien com a maxim 10 paquets en una hora
-
-iteracions = 10000;  % Podeu provar 10, 100, 1000, 10000
-lambda = 5;
-comptA2 = 0;
-
-for it = 1:iteracions
-    % Generacio de nclients ~ Poisson(lambda) amb transformada inversa
-    % (evitem funcions de toolboxes externs)
-    L = exp(-lambda);
-    p = 1;
-    nclients = 0;
-    while p > L
-        nclients = nclients + 1;
-        p = p * rand;
-    end
-    nclients = nclients - 1;
-
-    totalPaquets = 0;
-    for c = 1:nclients
-        numAleatori = rand;
-
-        if numAleatori <= 0.4
-            paquetsClient = 1;
-        elseif numAleatori <= 0.7
-            paquetsClient = 2;
-        elseif numAleatori <= 0.9
-            paquetsClient = 3;
-        else
-            paquetsClient = 4;
+        % Event A: les dues màquines estan funcionant
+        if funcionaM1 && funcionaM2
+            comptadorA = comptadorA + 1;
         end
 
-        totalPaquets = totalPaquets + paquetsClient;
+        % Event B: almenys una màquina està funcionant
+        if funcionaM1 || funcionaM2
+            comptadorB = comptadorB + 1;
+        end
     end
 
-    if totalPaquets <= 10
-        comptA2 = comptA2 + 1;
-    end
+    % 1.6 Càlcul de probabilitats en %
+    probabilitatA = (comptadorA / numsims) * 100;
+    probabilitatB = (comptadorB / numsims) * 100;
+
+    fprintf('EXERCICI 1 - numsims = %d\n', numsims);
+    fprintf('P(A) dues màquines funcionant: %.4f %%\n', probabilitatA);
+    fprintf('P(B) AL MENYS una màquina funcionant: %.4f %%\n\n', probabilitatB);
 end
-
-probA2 = 100 * comptA2 / iteracions;
-
-fprintf('EXERCICI 2\n');
-fprintf('iteracions = %d\n', iteracions);
-fprintf('P(A: com a maxim 10 paquets/hora) = %.4f %%\n', probA2);
--- Practica_5_exercisi2.m
+++ Practica_5_exercisi2.m
@@ -1,1 +1,61 @@
+% Nombre autores
+clear all
+close all
+clc
 
+% EXERCICI 2
+% SIMULACIÓ DE VARIABLES DISCRETES I LA SEVA PROBABILITAT
+
+valors_iteracions = [10 100 1000 10000];
+lambda = 5;
+
+for idx = 1:length(valors_iteracions)
+    iteracions = valors_iteracions(idx);
+
+    % 1.1 Inicialització del comptador de l'event A
+    comptadorA = 0;
+
+    % 1.3 Bucle principal
+    for i = 1:iteracions
+        % 1.4 nclients ~ Poisson(5) amb mètode de Knuth
+        L = exp(-lambda);
+        k = 0;
+        p = 1;
+
+        while p > L
+            k = k + 1;
+            p = p * rand();
+        end
+
+        nclients = k - 1;
+
+        % 1.5 Assignació de paquets per client
+        totalPaquets = 0;
+        for j = 1:nclients
+            numAleatori = rand();
+
+            if numAleatori <= 0.4
+                paquets = 1;
+            elseif numAleatori > 0.4 && numAleatori <= 0.7
+                paquets = 2;
+            elseif numAleatori > 0.7 && numAleatori <= 0.9
+                paquets = 3;
+            else
+                paquets = 4;
+            end
+
+            totalPaquets = totalPaquets + paquets;
+        end
+
+        % 1.6 Event A: s'envien com a màxim 10 paquets
+        if totalPaquets <= 10
+            comptadorA = comptadorA + 1;
+        end
+    end
+
+    % 1.7 Probabilitat en %
+    probabilitatA = (comptadorA / iteracions) * 100;
+
+    fprintf('EXERCICI 2 - iteracions = %d\n', iteracions);
+    fprintf('P(A) com a màxim 10 paquets urgents: %.4f %%\n\n', probabilitatA);
+end
